519025f34530a276e64007fd2d5de6a51d433b2b
Sebastian <jsortiz82@misena.edu.co>
2023-01-18 15:16:34 -0500

    Se crea funcionalidad guardar producto, se crea vista create y se testea clase productoController con logger

--- src/main/java/com/johanapp/ecommerce/controller/ProductoController.java
+++ src/main/java/com/johanapp/ecommerce/controller/ProductoController.java
@@ -1,15 +1,42 @@
 package com.johanapp.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.johanapp.ecommerce.model.Producto;
+import com.johanapp.ecommerce.model.Usuario;
+import com.johanapp.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class); //  se emplea para testear por consola
+	
+	@Autowired
+	private ProductoService productoservice;
 
 	@GetMapping("")
 	public String show() {
 		return "productos/show";
+	}
+	
+	@GetMapping("/create")
+	public String create() {
+		return "productos/create";
+	}
+	
+	@PostMapping("/save")
+	public String save(Producto producto) {
+		LOGGER.info("Este es el objeto producto {}",producto);
+		Usuario u = new Usuario(1, "", "", "", "", "", "", "");
+		producto.setUsuario(u);
+		productoservice.save(producto);
+		return "redirect:/productos";
 	}
 }
