2ed8be0ca71c6965de039eee6da38b7b1c4f23cb
Sebastian <jsortiz82@misena.edu.co>
2023-01-20 11:53:48 -0500

    Se crea vista mostrar producto al usuario

--- src/main/java/com/johanapp/ecommerce/controller/HomeController.java
+++ src/main/java/com/johanapp/ecommerce/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.johanapp.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.johanapp.ecommerce.service.ProductoService;
@@ -11,6 +14,8 @@ import com.johanapp.ecommerce.service.ProductoService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private ProductoService productoservice;
@@ -20,5 +25,11 @@ public class HomeController {
 		
 		model.addAttribute("Productos",productoservice.findAll());
 		return "usuario/home";
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		log.info("Id enviado como parametro {}",id);
+		return "usuario/productohome";
 	}
 }
