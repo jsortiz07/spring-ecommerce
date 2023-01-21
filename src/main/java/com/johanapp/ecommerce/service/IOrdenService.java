9dc6414264a51057902ad4d860171aabb8abb69f
Johan <jsortiz82@misena.edu.co>
2023-01-21 13:28:20 -0500

    Se crea metodo para generar el numero de la orden

--- src/main/java/com/johanapp/ecommerce/service/IOrdenService.java
+++ src/main/java/com/johanapp/ecommerce/service/IOrdenService.java
@@ -1,7 +1,11 @@
 package com.johanapp.ecommerce.service;
 
+import java.util.List;
+
 import com.johanapp.ecommerce.model.Orden;
 
 public interface IOrdenService {
+	
+	List<Orden> findAll();
 	Orden save(Orden orden);
 }
